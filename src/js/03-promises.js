b3b95081b3e540d2456555e42f45bc6ea82c0b47
m.backi <backimichal@gmail.com>
2023-03-27 18:23:46 +0200

    1
    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const delayInput = form.querySelector('input[name="delay"]');
+  const stepInput = form.querySelector('input[name="step"]');
+  const amountInput = form.querySelector('input[name="amount"]');
+
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    const promiseDelay = delay + (i - 1) * step;
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
